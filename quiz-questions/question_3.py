c140a44289b3c9c52c961218eaa11af4bc30b4b4
Leah Denham <ldenham3@github.com>
2017-04-04 19:20:51 -0700

    tests should be together *grumpy face*

--- quiz-questions/question_3.py
+++ quiz-questions/question_3.py
@@ -1,127 +1,13 @@
-import unittest
-
-
 def eldest_customer_per_state(customers):
-    """Eldest customer per state.
-
-    This function receives a dictionary containing states (as keys) and
-    customers for those states (as a list of dictionaries) and should
-    return a final dictionary containing the eldest customer per each state.
-    Example:
-
-    customers = {
-        'UT': [{
-            'name': 'Mary',
-            'age': 28
-        }, {
-            'name': 'John',  # Eldest
-            'age': 31
-        }],
-        'NY': [{
-            'name': 'Linda',  # Eldest
-            'age': 71
-        }, {
-            'name': 'Lisa',
-            'age': 25
-        }]
-    }
-    eldest_customer_per_state(customers)
-    >>>
-    {
-        'UT': {
-            'name': 'John',
-            'age': 31
-        },
-        'NY': {
-            'name': 'Linda',
-            'age': 71
-        }
-    }
-    """
     newDict = {}
     for state,customer_info in customers.items():
         oldest = 0
-        for person in customer_info:
-            if person['age'] > oldest:
-                newDict[state] = person
-                oldest = person['age']
-            else:
-                "howd you manage to get a customer younger than 0?"
+        if customer_info:
+            for person in customer_info:
+                if person:
+                    if person['age'] > oldest:
+                        newDict[state] = person
+                        oldest = person['age']
+        else:
+            newDict[state] = None
     return newDict
-
-
-
-class EldestCustomerTestCase(unittest.TestCase):
-    def test_eldest_customers(self):
-        """Should return the eldest customer per state."""
-        customers = {
-            'UT': [{
-                'name': 'Mary',
-                'age': 28
-            }, {
-                'name': 'John',
-                'age': 31
-            }, {
-                'name': 'Robert',
-                'age': 16
-            }],
-            'NY': [{
-                'name': 'Linda',
-                'age': 71
-            }, {
-                'name': 'Lisa',
-                'age': 25
-            }, {
-                'name': 'Daniel',
-                'age': 45
-            }],
-            'CA': [{
-                'name': 'Barbara',
-                'age': 15
-            }, {
-                'name': 'Paul',
-                'age': 18
-            }, {
-                'name': 'Helen',
-                'age': 29
-            }]
-        }
-        expected_result = {
-            'UT': {
-                'name': 'John',
-                'age': 31
-            },
-            'NY': {
-                'name': 'Linda',
-                'age': 71
-            },
-            'CA': {
-                'name': 'Helen',
-                'age': 29
-            }
-        }
-        self.assertEqual(eldest_customer_per_state(customers), expected_result)
-
-    def test_eldest_customers_with_empty_states(self):
-        customers = {
-            'UT': [{
-                'name': 'Mary',
-                'age': 28
-            }, {
-                'name': 'John',
-                'age': 31
-            }],
-            'NY': []
-        }
-        expected_result = {
-            'UT': {
-                'name': 'John',
-                'age': 31
-            },
-            'NY': None
-        }
-        self.assertEqual(eldest_customer_per_state(customers), expected_result)
-
-if __name__ == '__main__':
-    unittest.main()
-
--- quiz-questions/tests.py
+++ quiz-questions/tests.py
@@ -1,7 +1,145 @@
 import unittest
 from question_1 import question_1
+from question_2 import number_of_customers_per_state
+from question_3 import eldest_customer_per_state
 
-
+# QUESTION 1
 class QuestionsTestCase(unittest.TestCase):
     def test_question_1(self):
         self.assertEqual(question_1(), 30, "Question 1 answer FAILED")
+
+# QUESTION 2
+class NumberOfCustomersPerStateTestCase(unittest.TestCase):
+    def test_number_of_customers_per_state(self):
+        """Should return the correct number of customers per state."""
+        customers = {
+            'UT': [{
+                'name': 'Mary',
+                'age': 28
+            }, {
+                'name': 'John',
+                'age': 31
+            }, {
+                'name': 'Robert',
+                'age': 16
+            }],
+            'NY': [{
+                'name': 'Linda',
+                'age': 71
+            }],
+            'CA': [{
+                'name': 'Barbara',
+                'age': 15
+            }, {
+                'name': 'Paul',
+                'age': 18
+            }]
+        }
+        expected_result = {
+            'UT': 3,
+            'NY': 1,
+            'CA': 2
+        }
+        self.assertEqual(
+            number_of_customers_per_state(customers), expected_result)
+
+    def test_number_of_customers_per_state_with_blank_state(self):
+        """Should return the correct number of customers per state."""
+        customers = {
+            'UT': [{
+                'name': 'Mary',
+                'age': 28
+            }, {
+                'name': 'John',
+                'age': 31
+            }, {
+                'name': 'Robert',
+                'age': 16
+            }],
+            'NY': None,  # Be Careful! NY value is None
+            'CA': [{
+                'name': 'Barbara',
+                'age': 15
+            }, {
+                'name': 'Paul',
+                'age': 18
+            }]
+        }
+        expected_result = {
+            'UT': 3,
+            'NY': 0,
+            'CA': 2
+        }
+        self.assertEqual(
+            number_of_customers_per_state(customers), expected_result)
+
+# QUESTION 3
+class EldestCustomerTestCase(unittest.TestCase):
+    def test_eldest_customers(self):
+        customers = {
+            'UT': [{
+                'name': 'Mary',
+                'age': 28
+            }, {
+                'name': 'John',
+                'age': 31
+            }, {
+                'name': 'Robert',
+                'age': 16
+            }],
+            'NY': [{
+                'name': 'Linda',
+                'age': 71
+            }, {
+                'name': 'Lisa',
+                'age': 25
+            }, {
+                'name': 'Daniel',
+                'age': 45
+            }],
+            'CA': [{
+                'name': 'Barbara',
+                'age': 15
+            }, {
+                'name': 'Paul',
+                'age': 18
+            }, {
+                'name': 'Helen',
+                'age': 29
+            }]
+        }
+        expected_result = {
+            'UT': {
+                'name': 'John',
+                'age': 31
+            },
+            'NY': {
+                'name': 'Linda',
+                'age': 71
+            },
+            'CA': {
+                'name': 'Helen',
+                'age': 29
+            }
+        }
+        self.assertEqual(eldest_customer_per_state(customers), expected_result)
+
+    def test_eldest_customers_with_empty_states(self):
+        customers = {
+            'UT': [{
+                'name': 'Mary',
+                'age': 28
+            }, {
+                'name': 'John',
+                'age': 31
+            }],
+            'NY': []
+        }
+        expected_result = {
+            'UT': {
+                'name': 'John',
+                'age': 31
+            },
+            'NY': None
+        }
+        self.assertEqual(eldest_customer_per_state(customers), expected_result)
